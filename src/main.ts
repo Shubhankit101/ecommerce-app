866bd8b41270f5af7d643cd88d9e4942a7597285
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2025-03-06 23:47:10 +0530

    Added Header Component

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,22 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  /*
+  myFunction("asd");
+  
+  let a:number = 5;
+  console.log(a);
+
+  let log = function(message:string){
+    console.log(message);
+    return "asd";
+  }
+  function myFunction(message:string){
+    console.log("invoking my function");
+  }
+  let x = log("hello");
+  console.log(x);
+  
+  */
+
